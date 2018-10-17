723b034a0b663f48eee21e2b0ab6f5864101bb3e
Neuller <Neuller97@hotmail.com>
2018-10-17 20:17:48 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication-Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_Store.Startup))]
+namespace WebApplication_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
