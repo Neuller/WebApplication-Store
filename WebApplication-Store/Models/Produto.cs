19c6f43f08bdc8d59ec53a19a3aa3da12c2a4982
Neuller <Neuller97@hotmail.com>
2018-10-18 13:38:10 -0300

    Update

---
+++ WebApplication-Store/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_Store.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public DateTime UltimaCompra { get; set; }
+
+        public float Estoque { get; set; }
+
+        public string Comentario { get; set; }
+
+    }
+}
